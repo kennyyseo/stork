813949e6f79ca89efc8fba1b915138f23a6f451a
Kenny Seo <kennyyseo@gmail.com>
2020-10-15 21:55:05 -0700

    Make updates before changing NamesTable DND

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -29,10 +29,7 @@ const NavBar = (props) => {
         :
         <div>
             <nav className="navbar navbar-expand navbar-light bg-light">
-                <a className="navbar-brand" href="/">
-                    <img className='logo' src="https://i.imgur.com/49vugMb.png" alt="" />
-                    Stork
-                </a>
+                <Link to='/' className='navbar-brand' ><img className='logo' src="https://i.imgur.com/49vugMb.png" alt="" />Stork</Link>
                 <div id="navbarNav">
                     <ul className="navbar-nav">
                         <li>
